45441bb054b73939fabcba36dd14be0c2aeff4a3
Eric Holmes <eric@ejholmes.net>
2011-06-13 17:22:24 -0700

    Cleanup

--- util.h
+++ util.h
@@ -50,11 +50,9 @@ havedevice:
  * in the process */
 static inline void endian_swap(void *dest, const void *source, int length)
 {
-    unsigned char *dptr = (unsigned char *)dest;
-    unsigned char *sptr = (unsigned char *)source;
     int i;
     for (i = 0; i < length; i++) {
-        dptr[i] = (sptr[length - i]);
+        dest[i] = (sourcelength - i]);
     }
 }
 
