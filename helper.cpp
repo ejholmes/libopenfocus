9cabaa25f6aef0862811baed7101ad74dc5f22a4
Eric J. Holmes <eric@ejholmes.net>
2011-06-19 18:11:42 -0700

    Added debugging

--- helper.cpp
+++ helper.cpp
@@ -1,7 +1,14 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 #include "openfocus.h"
+
+#ifdef DEBUG
+#define DBG(...) printf(__VA_ARGS__)
+#else
+#define DBG(...)
+#endif
 
 int OpenFocus::Helper::ConnectBootloader()
 {
@@ -13,15 +20,19 @@ int OpenFocus::Helper::ConnectBootloader(bool timer)
     OpenFocus::Bootloader *bootloader = new OpenFocus::Bootloader();
 
     if (bootloader->Connect()) { /* Bootloader found */
+        DBG("Connected to bootloader\n");
         return 1;
     }
     else { /* Bootloader not found, try rebooting device into bootloader */
+        DBG("Attempting to connec to device before rebooting\n");
         OpenFocus::Device *device = new OpenFocus::Device();
         if (device->Connect()) {
+            DBG("Connected to device. Attempting to reboot into bootloader\n");
             device->RebootToBootloader();
             device->Disconnect();
             /* Wait 2000 ms */
             if (timer) {
+                DBG("Device is rebooting. Waiting 2 seconds before connecting\n");
                 for (time_t t = time(NULL) + 2; time(NULL) < t;) {}
                 return OpenFocus::Helper::ConnectBootloader();
             }
