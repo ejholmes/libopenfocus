4c4d7ccae018b89e246acdf4a96ad011f36b837f
Eric J. Holmes <eric@ejholmes.net>
2011-06-22 02:02:52 -0700

    Commandline tools on linux.
    Signed-off-by: Eric J. Holmes <eric@ejholmes.net>

---
+++ commandline/bootloader.cpp
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <openfocus.h>
+
+#include "intelhex.h"
+
+using namespace OpenFocus;
+
+void usage()
+{
+    printf("Usage: bootloader [OPTION]...\n");
+    printf("Control an openfocus device.\n\n");
+    
+    printf("  -u, --upload=FILE              write FILE to device flash memory. FILE should be in intel hex format.\n");
+}
+
+int main(int argc, char **argv)
+{
+    Bootloader *bootloader = new Bootloader();
+    int c;
+    opterr = 0;
+    while (1) {
+        static struct option long_options[] = {
+            {"help",        no_argument,       0, 'h'}, /* Help */
+            {"upload",      required_argument, 0, 'u'}, /* Uplaod firmware */
+            {0, 0, 0, 0}
+        };
+        
+        int option_index = 0;
+        c = getopt_long(argc, argv, "hu:", long_options, &option_index);
+
+        if (c == -1)
+            break;
+
+        switch (c) {
+            case 'h':
+                usage();
+                goto done;
+            case 'u':
+                if (!Helper::ConnectBootloader()) {
+                    fprintf(stderr, "Could not connect to bootloader\n");
+                    return -1;
+                }
+                {
+                    FILE *fp = NULL;
+                    fp = fopen(optarg, "r");
+                    if (fp) {
+                        record *records = NULL;
+                        flash *flashdata = NULL;
+                        records = IntelHexFile::Open(fp);
+                        flashdata = IntelHexFile::RecordsToFlashData(records);
+                        if (records) {
+                            if (bootloader->WriteFlash(flashdata->data, flashdata->size)) {
+                                printf("Uploaded %u bytes to device\n", flashdata->size);
+                            } else {
+                                fprintf(stderr, "An error occurred while writing flash data\n");
+                            }
+                        } else {
+                            fprintf(stderr, "No data to upload. Is the file in hex format?\n");
+                        }
+                        IntelHexFile::FreeRecords(records);
+                        IntelHexFile::FreeFlashData(flashdata);
+                    } else {
+                        fprintf(stderr, "Could not open file for reading\n");
+                        return -1;
+                    }
+                }
+                goto done;
+            default:
+                usage();
+                goto done;
+        }
+    }
+done:
+    if (bootloader && bootloader->IsConnected()) {
+        bootloader->Reboot();
+        bootloader->Disconnect();
+    }
+    return 0;
+}
--- commandline/openfocus.cpp
+++ commandline/openfocus.cpp
@@ -1,53 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <getopt.h>
 #include <openfocus.h>
 
 using namespace OpenFocus;
 
 void usage()
 {
-
+    printf("Usage: openfocus [OPTION]...\n");
+    printf("Control an openfocus device.\n\n");
+    
+    printf("  -l, --list                        list current connected devices.\n");
+    printf("  -r, --reverse                     reverse the direction that the stepper motor rotates.\n");
+    printf("  -p, --position                    print the current position.\n");
+    printf("  -t, --temperature                 print the current temperature.\n");
+    printf("  -s, --stop                        stop focuser movement.\n");
+    printf("  -m, --move=POSITION               move to POSITION.\n");
+    printf("  -c, --compensate=COEFFICIENT      start temperature compensation using COEFFICIENT for the temperature coefficient.\n");
 }
 
 int main(int argc, char **argv)
 {
     Device *device = new Device();
+    /* Try to connect to the device */
     if (!device->Connect()) {
-        printf("Could not connect to device!\n");
+        fprintf(stderr, "Could not connect to device!\n");
         return -1;
     }
     int c;
     opterr = 0;
-    while ((c = getopt(argc, argv, "lrptsm:c:")) != -1) {
+    while (1) {
+        static struct option long_options[] = {
+            {"help",        no_argument,       0, 'h'}, /* Help */
+            {"list",        no_argument,       0, 'l'}, /* List currently connected devices */
+            {"reverse",     no_argument,       0, 'r'}, /* Reverse motor direction */
+            {"position",    no_argument,       0, 'p'}, /* Print current position */
+            {"temperature", no_argument,       0, 't'}, /* Print current temperature */
+            {"stop",        no_argument,       0, 's'}, /* Stop movement */
+            {"move",        required_argument, 0, 'm'}, /* Move to position */
+            {"compensate",  required_argument, 0, 'c'}, /* Start temperature compensation */
+            {0, 0, 0, 0}
+        };
+        
+        int option_index = 0;
+        c = getopt_long(argc, argv, "hlrptsm:c:", long_options, &option_index);
+
+        if (c == -1)
+            break;
+
         switch (c) {
-            case 'l': /* List currently connected devices */
+            case 'h':
+                usage();
+                goto done;
+            case 'l':
                 {
+                    /* Enumerate all the connected devices and iterate through
+                     * the list */
                     device_info *devs = Device::EnumerateDevices();
                     for (devs; devs != NULL; devs = devs->next) {
                         printf("%s\n", devs->serial);
                     }
                 }
                 goto done;
-            case 'r': /* Reverse direction */
+            case 'r':
                 device->ReverseDirection(true);
                 break;
             case 'p':
                 printf("Position: %d\n", device->GetPosition());
                 goto done;
-            case 't': /* Get temperature */
+            case 't':
                 printf("Temperature: %f\n", device->GetTemperature());
                 goto done;
-            case 's': /* Stop/Halt */
+            case 's':
                 device->Halt();
                 goto done;
-            case 'm': /* Move */
+            case 'm':
                 {
                     unsigned short position = (unsigned short)atoi(optarg);
                     device->MoveTo(position);
                 }
                 goto done;
-            case 'c': /* Temperature compensation */
+            case 'c':
                 if (!device->CanTemperatureCompensate) {
                     printf("Device does not support temperature compensation\n");
                     goto done;
